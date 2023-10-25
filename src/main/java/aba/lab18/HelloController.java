bb3ef9d230cf003122f0f3cd35f44d44d42facd0
Svetlana <A.svet.mr@yandex.ru>
2023-10-25 12:29:54 +0300

    v1

--- src/main/java/aba/lab18/HelloController.java
+++ src/main/java/aba/lab18/HelloController.java
@@ -3,12 +3,44 @@ package aba.lab18;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import static javafx.application.Application.launch;
+
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private Label labelRez;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label textY;
+    @FXML
+    private Label textX;
+    @FXML
+    public  void  plus(){
+        double x = Double.parseDouble(textX.getText());
+        double y = Double.parseDouble(textY.getText());
+        double z = x+y;
+        labelRez.setText("Результат равен "+z);
+    }
+    @FXML
+    public  void  minus(){
+        double x = Double.parseDouble(textX.getText());
+        double y = Double.parseDouble(textY.getText());
+        double z = x-y;
+        labelRez.setText("Результат равен "+z);
+    }
+    @FXML
+    public  void  ymnosh(){
+        double x = Double.parseDouble(textX.getText());
+        double y = Double.parseDouble(textY.getText());
+        double z = x*y;
+        labelRez.setText("Результат равен "+z);
+    }
+    @FXML
+    public  void  delit(){
+        double x = Double.parseDouble(textX.getText());
+        double y = Double.parseDouble(textY.getText());
+        double z = x/y;
+        labelRez.setText("Результат равен "+z);
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
